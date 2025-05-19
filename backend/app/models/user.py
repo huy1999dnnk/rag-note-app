70f78d4c10429259905c69e3ab1cea8936fa1462
pham minh huy <phamminhhuy@phams-MacBook-Pro.local>
2025-05-19 21:20:49 +0700

    add address to users model for tesing migrate in prod

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -13,6 +13,7 @@ class User(Base):
     hashed_password = Column(String, nullable=True)
     is_active = Column(Integer, default=1)
     image = Column(String, nullable=True)
+    address = Column(String, nullable=True)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
 
